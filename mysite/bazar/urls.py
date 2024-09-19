6182821d2b556266608073271ae1051c069d6cc2
Leandro Sizilio <lesizilio@gmail.com>
2024-09-19 00:21:17 +0000

    aaaa

--- mysite/bazar/urls.py
+++ mysite/bazar/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "bazar"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('bazar/<int:pk>/', views.BazarView.as_view(), name='Bazar'),
 ]
--- mysite/bazar/views.py
+++ mysite/bazar/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from models import Bazar
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request,'bazar/index.html')
+
+class BazarView(View):
+    def get(self, request, *args, **kwargs):
+        id_bazar = kwargs ['pk']
+        bazar = get_object_or_404(Bazar, pk=id_bazar)
+        contexto = {'bazar': bazar}
+        return render(request, 'bazar/bazar.html', contexto)
